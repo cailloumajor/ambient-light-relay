9042324e50e7ae668ecb42479a2a7cfd25fca388
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-05-06 19:35:20 +0200

    chore: start experimenting with light sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <BH1750.h>
+
+const int relayPin = D0;
+
+BH1750 lightMeter;
+
+void setup()
+{
+    pinMode(relayPin, OUTPUT);
+
+    Serial.begin(9600);
+
+    Wire.begin(D2, D1);
+
+    lightMeter.begin();
+
+    Serial.println(F("BH1750 test begin"));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    float lux = lightMeter.readLightLevel();
+    Serial.print(F("Light: "));
+    Serial.print(lux);
+    Serial.println(F(" lx"));
+    digitalWrite(relayPin, lux < 20 ? HIGH : LOW);
+    delay(1000);
 }
